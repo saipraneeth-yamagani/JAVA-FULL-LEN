358f93999ad752cd09b67003853f5292ae4e0b2d
sai <praneethsai520@gmail.com>
2024-08-18 12:12:46 +0530

    new lines added

--- arrays.java
+++ arrays.java
@@ -1,15 +1,20 @@
-import java.util.*;
+// import java.util.*;
 public class arrays {
     public static void main(String[] args)
     {
-        Scanner sc = new Scanner(System.in);
-        int i =sc.nextInt();
-        System.out.println(i);
-        int numbers[] = {2,3,4,5,6,7,8,9};
-        System.out.println(numbers.length);
+       int numbers[] = {1,4,8,7,0};
 
+        for(int i=0;i<5;i++)
+        {
+            System.out.println(numbers[i]);
+        }
 
-        sc.close();
+        // for(int i=0;i<=5;i++)
+        // {
+        //     System.out.println(i);
+        // }
+
+        // sc.close();
     }
     
     
--- forloop.java
+++ forloop.java
@@ -1,15 +1,19 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class forloop {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number: ");
-        int number = sc.nextInt();
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter a number: ");
+        // int number = sc.nextInt();
 
-        for (int i = 1; i <number; i++) {
-            System.out.print(i + " ");
+        for (int i = 0; i <=5; i++) {
+            System.out.println(i);
+            System.out.println("Sai praneeth");
         }
-        sc.close();
+
+
+
+        // sc.close();
     }
 }
