3e603db3c968c31f3f8fb0f0ac209157ff73874d
sai2 <praneethsai520@gmail.com>
2024-08-21 18:47:00 +0530

    new code

--- DSA_ANIMUTHYAM/arrays_loopsr.java
+++ DSA_ANIMUTHYAM/arrays_loopsr.java
@@ -1,6 +1,7 @@
 package DSA_ANIMUTHYAM;
 
 public class arrays_loopsr {
+    @SuppressWarnings("unused")
     public static void main(String[] args) {
         // int arr[] = {1,2,4,1,7,3,1};
 
@@ -14,35 +15,40 @@ public class arrays_loopsr {
         // System.out.println(count);
 
 
-            int arry[] ={ 2, 4, 6,14, 9, 3, 15,27};
-            int count =0;
-            for(int i=0;i<arry.length;i++)
-            {
-                int temp = arry[i];
-                if(temp % 2 ==0 || temp % 3 ==0)
-                {
-                    count++;
-                }
-            }
-            System.out.println(count);
+            // int arry[] ={ 2, 4, 6,14, 9, 3, 15,27};
+            // int count =0;
+            // for(int i=0;i<arry.length;i++)
+            // {
+            //     int temp = arry[i];
+            //     if(temp % 2 ==0 || temp % 3 ==0)
+            //     {
+            //         count++;
+            //     }
+            // }
+            // System.out.println(count);
 
 
 
 // or - ||
 // and - &&
-    int arrys[] ={ 2, 4, 6,14, 9, 3, 15,27};
-            int counts =0;
-            for(int i=0;i<arry.length;i++)
-            {
-                int temp = arry[i];
-                if(temp % 2 ==0 && temp % 3 ==0)
-                {
-                    counts++;
-                }
-            }
-            System.out.println(counts);
+    // int arrys[] ={ 2, 4, 6,14, 9, 3, 15,27};
+    //         int counts =0;
+    //         for(int i=0;i<arry.length;i++)
+    //         {
+    //             int temp = arrys[i];
+    //             if(temp % 2 ==0 && temp % 3 ==0)
+    //             {
+    //                 counts++;
+    //             }
+    //         }
+    //         System.out.println(counts);
+        if ((1<5 && 5>3) & (3>4 && 8>3) ){
+            System.out.println("true boom");
+        } else {
+            System.out.println("false");
 
-
+        }
+    }
 
 
 
@@ -51,4 +57,4 @@ public class arrays_loopsr {
 
 
     }
-}
+
