68b0025efa1b2e566c0033fa585eb984552f4d8c
sai2 <praneethsai520@gmail.com>
2024-10-07 15:02:07 +0530

    hameed

--- DSA_ANIMUTHYAM/functions.java
+++ DSA_ANIMUTHYAM/functions.java
@@ -1,19 +1,16 @@
 package DSA_ANIMUTHYAM;
 
-
 public class functions {
-    
-    public static void main(String[] args) {
-        
-            fun("saikiran", 5);
-            fun("sanjuu", 5);
 
+    public static void main(String[] args) {
+
+        fun("saikiran", 5);
+        fun("hameed", 5);
 
     }
 
-
-    public static void  fun(String s, int n){
-        for(int i=0;i<n;i++){
+    public static void fun(String s, int n) {
+        for (int i = 0; i < n; i++) {
             System.out.println(s);
         }
     }
